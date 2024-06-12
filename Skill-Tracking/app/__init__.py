d7de45ee7149c39439de140ae80928d2ed7dd6c1
Cooper Martin <156128377+CoopToss@users.noreply.github.com>
2024-06-12 09:41:22 -0400

    Update __init__.py

--- Skill-Tracking/app/__init__.py
+++ Skill-Tracking/app/__init__.py
@@ -14,7 +14,8 @@ def create_app():
     db.init_app(app)
     login_manager.init_app(app)
 
-    from .routes import routes
-    app.register_blueprint(routes)
-
+    with app.app_context():
+        from .routes import routes
+        app.register_blueprint(routes)
+        
     return app
