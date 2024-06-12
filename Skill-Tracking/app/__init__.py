8a6104310d8c7ddb6e7aaa8f1d462caa963e9f81
Cooper Martin <156128377+CoopToss@users.noreply.github.com>
2024-06-12 17:50:18 -0400

    Update __init__.py

--- Skill-Tracking/app/__init__.py
+++ Skill-Tracking/app/__init__.py
@@ -11,14 +11,13 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    db.init_app(app)
+    login_manager.init_app(app)
+
     with app.app_context():
         from .routes import routes
         app.register_blueprint(routes, url_prefix='/')
 
-        db.app = app
-        db.init_app(app)
-        db.create_all()
-    
     @login_manager.user_loader
     def load_user(user_id):
         from .models import User  # Importing inside the function to avoid circular imports
