dba8642bfb11dc4dc905a50b92ceeca170e4c90d
Cooper Martin <156128377+CoopToss@users.noreply.github.com>
2024-06-12 12:26:51 -0400

    Add files via upload

--- Skill-Tracking/manage.py
+++ Skill-Tracking/manage.py
@@ -1,11 +1,10 @@
-from flask import Flask
-from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from app import create_app, db
 
 app = create_app()
-migrate = Migrate(app, db)
 manager = Manager(app)
+migrate = Migrate(app, db)
 
 manager.add_command('db', MigrateCommand)
 
--- Skill-Tracking/wsgi.py
+++ Skill-Tracking/wsgi.py
@@ -5,3 +5,4 @@ app.secret_key = 'cooperwashere'
 
 if __name__ == "__main__":
     app.run()
+
