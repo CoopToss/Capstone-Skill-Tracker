5a102faa9d2207171b113a63863bee65e093d3c5
Cooper Martin <156128377+CoopToss@users.noreply.github.com>
2024-06-13 09:26:38 -0400

    Update config.py

--- Skill-Tracking/config.py
+++ Skill-Tracking/config.py
@@ -4,7 +4,7 @@ SQLALCHEMY_DATABASE_URI = 'postgresql://cooper:cooper12@localhost:5432/skill-tra
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('cooperwashere') or "hello"
+    SECRET_KEY = os.environ.get('cooperwashere')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
