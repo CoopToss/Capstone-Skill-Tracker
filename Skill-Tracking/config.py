adb2fd6638b295e3ca55360e98694ac6cee48fe4
Cooper Martin <156128377+CoopToss@users.noreply.github.com>
2024-06-13 13:45:33 -0400

    Update config.py

--- Skill-Tracking/config.py
+++ Skill-Tracking/config.py
@@ -1,15 +1,10 @@
+# config.py
 import os
 
-# Define the base directory of the project
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    # Secret key for sessions and CSRF protection
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'cooperwashere'
-
-    # PostgreSQL database URI
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'postgresql://cooper:cooper12@localhost:5433/skill-tracking'
-
-    # Disable tracking modifications to avoid unnecessary overhead
     SQLALCHEMY_TRACK_MODIFICATIONS = False
